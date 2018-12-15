a51b3411a0a3461515a2f451111bf9053b5aa429
Kevin Waddle <bonzobavaria@gmail.com>
2018-12-14 22:52:38 -0600

    update replicate function

--- List.js
+++ List.js
@@ -1,14 +1,11 @@
 const R = require('ramda')
 
-const replicate = (times, notes) => {
+const replicate = (times, value) => {
   const output = []
-  for (i = 0; i < notes.length; i++) {
-    for (j = 0; j < times; j++) {
-      output.push(notes[i])
-    }
+  for (i = 0; i < times; i++) {
+    output.push(value)
   }
   return output
 }
 
 exports.replicate = R.curry(replicate)
-
